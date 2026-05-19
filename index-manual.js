0e6c0e8ecfc87117c64e78840e5ed604eabf6e16
Muhammad Wahyu Pratama <muhammad123wahyu123@gmail.com>
2026-05-19 16:01:37 +0700

    Add files via upload

--- index-manual.js
+++ index-manual.js
@@ -1,27 +1,25 @@
-
-const readline = require("node:readline")
+import readline from "node:readline";
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-})
+  input: process.stdin,
+  output: process.stdout,
+});
 
 rl.question("Masukan Tanggal dengan Format (DD-MM-YYYY) :", (tanggal) => {
-    const pemisah = tanggal.split("-")
+  const pemisah = tanggal.split("-");
 
-    if (pemisah.length !== 3) {
-        console.log("Format Tanggal Salah!!")
-        rl.close()
-        return
-    }
-    const hari = pemisah[0]
-    const bulan = pemisah[1]
-    const tahun = pemisah[2]
+  if (pemisah.length !== 3) {
+    console.log("Format Tanggal Salah!!");
+    rl.close();
+    return;
+  }
+  const hari = pemisah[0];
+  const bulan = pemisah[1];
+  const tahun = pemisah[2];
 
-    if (hari.length !== 2 || bulan.length !== 2 || tahun.length !== 4) {
-        console.log("Format Tanggal Salah")
-    } else {
-        console.log(`Tanggal : ${hari}/${bulan}/${tahun}`)
-    } rl.close()
-}
-)
-
+  if (hari.length !== 2 || bulan.length !== 2 || tahun.length !== 4) {
+    console.log("Format Tanggal Salah");
+  } else {
+    console.log(`Tanggal : ${hari}/${bulan}/${tahun}`);
+  }
+  rl.close();
+});
--- index.js
+++ index.js
@@ -1,19 +1,20 @@
-const readline = require("node:readline");
-const moment = require("moment")
+import readline from "node:readline";
+import moment from "moment";
 
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+  input: process.stdin,
+  output: process.stdout,
 });
 
-rl.question("Masukan tanggal dengan format (DD-MM-YYYY) :", (tanggal) => {
-    const benar = moment(tanggal, "DD-MM-YYYY", true);
-    if (benar.isValid()) {
-        const hasil = benar.format("DD/MM/YYYY");
-        console.log("hasil ", hasil);
-    } else {
-        console.log("format tanggal salah!!");
-    }
-    rl.close();
-}
-)
+rl.question("Masukan tanggal dengan format (DD-MM-YYYY): ", (tanggal) => {
+  const benar = moment(tanggal, "DD-MM-YYYY", true);
+
+  if (benar.isValid()) {
+    const hasil = benar.format("DD/MM/YYYY");
+    console.log("hasil", hasil);
+  } else {
+    console.log("format tanggal salah!!");
+  }
+
+  rl.close();
+});
