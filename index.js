ee94f19d8fef9016824449834a1fe5967ac9cd14
Muhammad Wahyu Pratama <muhammad123wahyu123@gmail.com>
2026-05-19 14:47:17 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+const readline = require("node:readline");
+const moment = require("moment")
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question("Masukan tanggal dengan format (DD-MM-YYYY) :", (tanggal) => {
+    const benar = moment(tanggal, "DD-MM-YYYY", true);
+    if (benar.isValid()) {
+        const hasil = benar.format("DD/MM/YYYY");
+        console.log("hasil ", hasil);
+    } else {
+        console.log("format tanggal salah!!");
+    }
+    rl.close();
+}
+)
