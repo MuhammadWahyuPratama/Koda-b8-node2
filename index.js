a1206417b9611e9f972c9b0b1a524317ae49b3d4
Muhammadwahyu1212 <muhammad123wahyu123@gmail.com>
2026-05-27 21:01:20 +0700

    refactor date formatter and setup project dependencies

--- index.js
+++ index.js
@@ -6,15 +6,20 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
-rl.question("Masukan tanggal dengan format (DD-MM-YYYY): ", (tanggal) => {
-  const benar = moment(tanggal, "DD-MM-YYYY", true);
+export function formatDate(tanggal) {
+ const benar = moment(tanggal, "DD-MM-YYYY",true);
+ if (!benar.isValid()) {
+  return null;
+}
+return benar.format("DD/MM/YYYY");
+};
 
-  if (benar.isValid()) {
-    const hasil = benar.format("DD/MM/YYYY");
-    console.log("hasil", hasil);
+rl.question("Masukan tanggal dengan format (DD-MM-YYYY): ", (tanggal) => {
+  const hasil = formatDate(tanggal);
+  if (hasil) {
+        console.log("hasil", hasil);
   } else {
     console.log("format tanggal salah!!");
   }
-
   rl.close();
 });
