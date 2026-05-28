c0d2ffd2ae454f69836c8c9a6e2b8100f257bb6c
Muhammadwahyu1212 <muhammad123wahyu123@gmail.com>
2026-05-28 07:40:20 +0700

    docs : add jsdocs in formatDate and formatManualDate

--- utils/formatDate.js
+++ utils/formatDate.js
@@ -1,5 +1,13 @@
 import moment from "moment";
 
+/**
+ * Formats date using moment.js
+ * from DD-MM-YYYY to DD/MM/YYYY.
+ *
+ * @param {string} tanggal date input
+ * @returns {string|null} formatted date or null if invalid
+ */
+
 export function formatDate(tanggal) {
   const benar = moment(
     tanggal,
--- utils/formatManualDate.js
+++ utils/formatManualDate.js
@@ -1,3 +1,11 @@
+/**
+ * Formats date manually
+ * from DD-MM-YYYY to DD/MM/YYYY.
+ *
+ * @param {string} tanggal date input
+ * @returns {string|null} formatted date or null if invalid
+ */
+
 export function formatManualDate (tanggal) {
  const pemisah = tanggal.split("-");
 if(pemisah.length !== 3) {
